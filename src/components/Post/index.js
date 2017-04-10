35af6db5317377605dbbdefb113664d0103c6089
Eugene Choe <eugene.choe@gmail.com>
2017-04-10 12:47:33 -0700

    Stylize Post page

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   Text,
   ScrollView,
 } from 'react-native';
+import styles from './styles';
 
 class Post extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    }
+  }
+
+  highlightText(text) {
+    return <Text style={styles.highlight}>{text}</Text>;
   }
 
   render() {
@@ -17,46 +19,10 @@ class Post extends Component {
       <ScrollView 
         style={styles.container}
       >
-        <Text style={styles.welcome}>
-          Reddit Native
-        </Text>
-        <Text style={styles.welcome}>
-          {this.props.item.data.domain}
-        </Text>
-        <Text style={styles.welcome}>
-          {this.props.item.data.permalink}
-        </Text>
-        <Text style={styles.welcome}>
-          {this.props.item.data.title}
-        </Text>
+        <Text>{this.highlightText(this.props.item.data.author)} posted a link to {this.highlightText(this.props.item.data.subreddit_name_prefixed)} from {this.highlightText(this.props.item.data.domain)}. It has a score of {this.highlightText(this.props.item.data.score)} and {this.highlightText(this.props.item.data.num_comments)} comments!{"\n\nHere's the title(gore):\n"}{this.props.item.data.title + "\n\nURL:\n"}{this.props.item.data.url}</Text>
       </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    paddingTop: 20
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  searchRow: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    paddingHorizontal: 10,
-    paddingTop: 20
-  }
-});
 
 export default Post;
