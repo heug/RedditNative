4571645b5c4c3ff117771f57470cfbea2388db5f
Eugene Choe <eugene.choe@gmail.com>
2017-04-10 01:06:13 -0700

    Implement redux-persist

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, AsyncStorage } from 'react-native';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import allReducers from './reducers';
 import Routes from './config/routes';
 
-const store = createStore(allReducers, applyMiddleware(thunk));
+const store = createStore(allReducers, compose(applyMiddleware(thunk), autoRehydrate()));
+persistStore(store, {storage: AsyncStorage});
 
 export default class Container extends Component {
   render() {
